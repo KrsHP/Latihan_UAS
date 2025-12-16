7fec42462e77d52e37bdfe4c2b251589f367fc94
KrsHP <kresna.hono.putro@student.upi.edu>
2025-12-16 21:04:31 +0700

    Adding procedure login and CSV user

--- login.py
+++ login.py
@@ -1,5 +1,33 @@
 #Penulis Kresna
 import pandas as pd
 
-csvFileUser = pd.read_csv("user.csv")
-print(csvFileUser.head())
+df_user = pd.read_csv("user.csv")
+
+
+def login():
+    while True:
+
+        print("=== Selamat Datang ===")
+        print()
+        usernameInput = input("Masukkan Username: ")
+        passwordInput = input("Masukkan Password: ")
+
+        user = df_user[
+            (df_user["username"] == usernameInput) &
+            (df_user["password"] == passwordInput)
+        ]
+        
+        if not user.empty:
+            role = int(user.iloc[0]["role"])
+            print("\nLogin Berhasil\n")
+            
+            if role == 2:
+                print("Role: Admin")
+                break
+            elif role == 1:
+                print("Role: User")
+                break
+            else:
+                print("Username atau Password salah. Silahkan coba lagi\n")
+                
+login()
